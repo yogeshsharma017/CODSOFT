73ccb4fdce167decd6d03adeedafaa0eacebd15a
Yogesh Sharma <96739821+psylence07@users.noreply.github.com>
2023-09-11 20:34:20 +0530

    Update main.js

--- calculator/main.js
+++ calculator/main.js
@@ -1,1 +1,32 @@
+let input = "";
+
+function appendNumber(number) {
+    input += number;
+    updateDisplay();
+}
+
+function appendOperator(operator) {
+    input += " " + operator + " ";
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        const result = eval(input);
+        input = result;
+        updateDisplay();
+    } catch (error) {
+        input = "Error";
+        updateDisplay();
+    }
+}
+
+function clearInput() {
+    input = "";
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById("result").value = input;
+}
 
